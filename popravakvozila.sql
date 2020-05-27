b74ac9b96ea492daa92bc6b2544ad44582acc628
Edunova Mateo Jović <mateojovic22@edunova.hr>
2020-05-27 14:11:44 +0200

    izmjena baze za servis vozila

--- popravakvozila.sql
+++ popravakvozila.sql
@@ -6,30 +6,30 @@ use popravakvozila;
 
 create table vozilo(
     sifra int not null primary key auto_increment,
-    registracijska_oznaka varchar(20),
-    marka varchar(50),
-    boja varchar(20),
-    godina_proizvodnje datetime,
-    korisnik int
+    registracijska_oznaka varchar(20) not null,
+    marka varchar(50) not null,
+    boja varchar(20) not null,
+    godina_proizvodnje datetime not null,
+    korisnik int not null
 );
 
 create table servis(
     sifra int not null primary key auto_increment,
-    datum_servisa datetime,
-    cijena decimal(10,2),
+    datum_servisa datetime not null,
+    cijena decimal(10,2) not null,
     broj_servisa_auta int,
-    vozilo int,
-    korisnik int
+    vozilo int not null,
+    korisnik int not null
 
 );
 
 create table korisnik(
     sifra int not null primary key auto_increment,
     OIB int,
-    ime varchar(20),
-    prezime varchar(20),
+    ime varchar(20) not null,
+    prezime varchar(20) not null,
     adresa varchar(30),
-    mobitel int
+    mobitel int not null
 );
 
 
