86a2120ad04de3fb2d18bd199b486a856d54f933
Edunova Mateo Jović <mateojovic22@edunova.hr>
2020-05-27 14:08:48 +0200

    nadopuna

--- popisKnjiga.sql
+++ popisKnjiga.sql
@@ -7,37 +7,39 @@ create table knjiznica(
     sifra int not null primary key auto_increment,
     naziv varchar(50) not null,
     adresa varchar(50) not null,
-    djelatnik int,
-    korisnik int,
-    radno_vrijeme varchar(50),
-    knjiga int
+    djelatnik int not null,
+    korisnik int not null,
+    radno_vrijeme varchar(50) not null,
+    knjiga int not null
 
 );
 
 create table djelatnik(
     sifra int not null primary key auto_increment,
-    knjiznica int,
+    knjiznica int not null,
     ime varchar(20) not null,
     prezime varchar(20) not null,
     ugovor varchar(50),
+    mobitel int not null,
     datom_pocetka_rada datetime
 );
 
 create table korisnik(
     sifra int not null primary key auto_increment,
-    knjiznica int,
+    knjiznica int not null,
     ime varchar(20) not null,
     prezime varchar(20) not null,
     oib int,
+    mobitel int not null,
     adresa varchar(50)
 
 );
 
 
 create table knjiga(
-    korisnik int,
-    knjiznica int,
-    naslov varchar(50),
+    korisnik int not null,
+    knjiznica int not null,
+    naslov varchar(50) not null,
     izdanja varchar(50),
     datum_posudbe datetime,
     cijena_kasnine decimal(8,2)
